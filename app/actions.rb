0461cd443d354a7a0c8df12569a58ec7c31742be
dorianwolf <dorian.wolf@gmail.com>
2015-11-09 17:44:45 +0000

    basics finished (list, post, etc)

--- app/actions.rb
+++ app/actions.rb
@@ -7,3 +7,17 @@ get '/songs' do
   @songs = Song.all
   erb :'songs/index'
 end
+
+post '/songs' do
+  @song = Song.new(
+  author: params[:author],
+  title: params[:title],
+  url: params[:url]
+  )
+  @song.save
+  redirect '/songs'
+end
+
+get '/songs/new' do
+  erb :'songs/new'
+end
